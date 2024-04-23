3b5bbe4c032f5124d10a898568ea6bd18a88605a
Serafim <25maillist25@list.ru>
2024-04-23 14:36:25 +0300

    delite time.sleep

--- pages/product_page.py
+++ pages/product_page.py
@@ -24,7 +24,6 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         basket_btn = self.browser.find_element(*ProductPageLocators.BASKET_BTN)
         basket_btn.click()
-        time.sleep(1)
         self.solve_quiz_and_get_code()
 
     def should_be_product_page(self):
@@ -37,12 +36,12 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_MESSAGE), "Product message is not presented"
 
     def should_be_correct_product(self):
-        assert (self.browser.find_element(*ProductPageLocators.ACTUAL_PRODUCT).text in
+        assert (self.browser.find_element(*ProductPageLocators.ACTUAL_PRODUCT).text ==
                 self.browser.find_element(*ProductPageLocators.CORRECT_PRODUCT).text), "Product is incorrect"
 
     def should_be_price_message(self):
         assert self.is_element_present(*ProductPageLocators.PRICE_MESSAGE), "Price message is not presented"
 
     def should_be_correct_price(self):
-        assert (self.browser.find_element(*ProductPageLocators.ACTUAL_PRICE).text in
+        assert (self.browser.find_element(*ProductPageLocators.ACTUAL_PRICE).text ==
                 self.browser.find_element(*ProductPageLocators.CORRECT_PRICE).text), "Price is incorrect"
