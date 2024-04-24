a945c0fff819f45a7f3a721b7da24f4c292c6d55
Serafim <25maillist25@list.ru>
2024-04-24 16:17:28 +0300

    add BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -18,3 +14,8 @@ class ProductPageLocators():
     PRICE_MESSAGE = (By.CSS_SELECTOR, ".alertinner p:nth-child(1)")
     ACTUAL_PRICE = (By.CSS_SELECTOR, "#messages :nth-child(1)>strong")
     CORRECT_PRICE = (By.CSS_SELECTOR, ".product_main p")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
