cde759cb0cd679de40566801addc0893cd086d0d
Serafim <25maillist25@list.ru>
2024-04-27 13:04:30 +0300

    add mark.need_review and pytest.ini

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFF
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en",
+    parser.addoption('--language', action='store', default=None,
                      help="Choose language")
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
@@ -19,10 +20,12 @@ def browser(request):
         browser_name = request.config.getoption("browser_name")
         if browser_name == "chrome":
             print(f"\nstart chrome browser with language {user_language}")
-            browser = webdriver.Chrome()
+            browser = webdriver.Chrome(options=options)
         elif browser_name == "firefox":
             print(f"\nstart firefox browser with language {user_language}")
-            browser = webdriver.Firefox()
+            options_ff = OptionsFF()
+            options_ff.set_preference("intl.accept_languages", user_language)
+            browser = webdriver.Firefox(options=options_ff)
         else:
             raise pytest.UsageError("--browser_name should be chrome or firefox")
     else:
--- test_product_page.py
+++ test_product_page.py
@@ -5,6 +5,7 @@ from .pages.basket_page import BasketPage
 import time
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6",
                                          pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
@@ -51,6 +52,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -60,6 +62,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -90,6 +93,7 @@ class TestUserAddToBasketFromProductPage():
         product_page = ProductPage(browser, browser.current_url)
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
